2e8934e2ba8cfac374e8188da70d552ad4614520
hezzy1 <Oayeni88@gmail.com>
2023-10-12 05:28:05 +0100

    my profile

---
+++ script.js
@@ -0,0 +1,23 @@
+
+document.getElementById('showSkill').addEventListener('click', function () {
+    document.querySelector('.skills').style.display = 'block';
+});
+document.getElementById('hideSkill').addEventListener('click', function () {
+    document.querySelector('.skills').style.display = 'none';
+});
+
+
+document.getElementById('showInfo').addEventListener('click', function () {
+    document.querySelector('.profile-info').style.display = 'block';
+});
+document.getElementById('hideInfo').addEventListener('click', function () {
+    document.querySelector('.profile-info').style.display = 'none';
+});
+
+
+document.getElementById('showExp').addEventListener('click', function () {
+    document.querySelector('.Experience').style.display = 'block';
+});
+document.getElementById('hideExp').addEventListener('click', function () {
+    document.querySelector('.Experience').style.display = 'none';
+});
